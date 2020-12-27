070f713376abeb6fc3aea111b9b0f3844339d90c
shayzaran <selahattin_hayzaran@hotmail.com>
2020-12-27 15:52:43 +0300

    collection items added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-import "./App.css";
+import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HatsPage</h1>
-    </div>
-  );
-};
+import "./App.css";
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage}></Route>
-      <Route path="/hats" component={HatsPage}></Route>
+      <Route path="/shop" component={ShopPage}></Route>
     </Switch>
   );
 }
