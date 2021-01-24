efa69c31414f6a78fbf222c8d4370bf5a6d648ee
shayzaran <selahattin_hayzaran@hotmail.com>
2021-01-24 15:49:51 +0300

    button type added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -61,7 +61,11 @@ export default class SignIn extends Component {
             <CustomButton type="submit" value="Submit Form">
               Sign In
             </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+            <CustomButton
+              type="button"
+              onClick={signInWithGoogle}
+              isGoogleSignIn
+            >
               Sign In With Google
             </CustomButton>
           </div>
